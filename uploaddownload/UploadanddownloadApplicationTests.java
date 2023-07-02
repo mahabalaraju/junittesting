dc31b6455ddcc6f99df760e479b5b276608166e7
H  Mahabalaraju <mahabalaraj4@gmail.com>
2023-07-02 11:13:45 +0530

    Add files via upload

---
+++ uploaddownload/UploadanddownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uploaddownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadanddownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
